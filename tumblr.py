8f198f09091ba55ec70926c6a0977791330d4ac8
chiefexb <chief_exb@mail.ru>
2016-11-09 23:28:51 +0300

    1

--- tumblr.py
+++ tumblr.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 #coding: utf8
+import requests
 def main():
     url="https://www.tumblr.com/oauth/request_token"
+    r=requests.post(url, data={'key':akey,'secret':sec}) 
 if __name__ == "__main__":
     main()
